1a7ea4123e01582cf6c1c0175ad2f8009c0bb274
abdulqdos <abdulqdosalabinie@gmail.com>
2025-07-20 15:06:48 +0200

    Fix Style

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button() {
     return (
-        <button className="bg-secondary text-white px-4 py-2 w-fit hover:bg-secondary-dark transition duration-300 cursor-pointer">
+        <button className="bg-secondary text-white px-4 py-2  w-fit hover:bg-secondary-dark transition duration-300 cursor-pointer">
             Button
         </button>
     );
--- src/Components/MainContent.tsx
+++ src/Components/MainContent.tsx
@@ -3,7 +3,7 @@ import Sidebar from "./Sidebar";
 
 export default function MainContent() {
     return (
-        <div className="mx-80 flex lg:flex-row flex-col flex-grow items-center justify-center">
+        <div className="mx-40 flex lg:flex-row flex-col flex-grow items-center justify-center">
            <div className="flex-3/4">
                <Body  />
            </div>
