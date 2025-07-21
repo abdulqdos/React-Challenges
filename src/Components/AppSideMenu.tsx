7eed76c7165f42681a319103041ede25e42d048b
abdulqdos <abdulqdosalabinie@gmail.com>
2025-07-21 11:40:35 +0200

    List challenge

--- src/Components/AppSideMenu.tsx
+++ src/Components/AppSideMenu.tsx
@@ -1,10 +1,5 @@
 import Sidebar from "./Sidebar";
 
 export default function AppSideMenu({showCategory = false}) {
-    console.log("AppSideMenu rendered with showCategory:", showCategory);
-  if (showCategory) {
-    return <Sidebar />;
-  } else {
-    return null ;
-  }
+    return showCategory ? <Sidebar /> : null;  
 }
